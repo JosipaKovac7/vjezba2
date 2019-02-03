b8d91857779ce39fdf76ec66ac5281055400ca14
Josipa Kovač <josipa.kovac@mev.hr>
2019-02-03 10:59:01 +0100

    novo

---
+++ vjezba2/GeometrijskiLikovi.cs
@@ -0,0 +1,22 @@
+﻿namespace vjezba2
+{
+    abstract class GeometrijskiLikovi
+    {
+        public abstract double opseg();
+        public abstract double povrsina();
+
+        private double a;
+        public double A
+        {
+            get{
+                return a;
+            }
+
+            set {
+
+                a = value;
+            }
+            
+        }
+    }
+}
